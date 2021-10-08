d93de4f8404352b11bb6a5887b56ca12382fc992
s8t9co <christian.bellet@vd.ch>
2021-10-08 15:06:08 +0200

    feat: Use environment variables

--- python/scripts/dbConnector.py
+++ python/scripts/dbConnector.py
@@ -1,3 +1,4 @@
+import os
 import mysql.connector
 from mysql.connector import Error
 
@@ -5,10 +6,12 @@ def connect():
     """ Connect to MySQL database """
     conn = None
     try:
-        conn = mysql.connector.connect(host='db',
-                                       database='app_db',
-                                       user='db_user',
-                                       password='db_user_pass')
+        conn = mysql.connector.connect(
+            host='db',
+            database=os.getenv('MYSQL_DATABASE'),
+            user=os.getenv('MYSQL_USER'),
+            password=os.getenv('MYSQL_PASSWORD')
+        )
         if conn.is_connected():
             print('Connected to MySQL database')
 
